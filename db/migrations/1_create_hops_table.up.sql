e10aca298fba2535452d0af1e07bcbeb28881c19
Leon Bogaert <leon@tim-online.nl>
2017-10-05 00:32:04 +0200

    Ideas for database

---
+++ db/migrations/1_create_hops_table.up.sql
@@ -0,0 +1,23 @@
+hops
+
+id
+name
+country (ISO3)
+description
+usage (bittering, aroma, both)
+hop_storage_index
+alpha_acid_percentage_max
+alpha_acid_percentage_min
+beta_acid_percentage_max
+beta_acid_percentage_min
+
+??
+co_humolone_percentage
+total_oil_percentage
+b_pinene_percentage
+myrcene_percentage
+linalool_percentage
+caryophyllene_percentage
+farnesene_percentage
+humulene_percentage
+geraniol_percentage
---
+++ db/migrations/2_create_hops_substitutes_table.up.sql
@@ -0,0 +1,6 @@
+hop_substitutes
+
+hop_a_id
+hop_b_id
+match
+source
